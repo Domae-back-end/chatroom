6e424534e7365e402ed39099aa463f374de23b7a
Domae-back-end <ty_ty123@naver.com>
2023-05-21 22:02:51 +0900

    create: 메인 controller 수정과 html 파일 생성 기본 페이지, 방 관리 와 방 목록 생성.

--- src/main/java/com/chatroom/chatroom/controller/MainController.java
+++ src/main/java/com/chatroom/chatroom/controller/MainController.java
@@ -1,7 +1,37 @@
 package com.chatroom.chatroom.controller;
 
+import com.chatroom.chatroom.service.ChatRoomService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private ChatRoomService chatRoomService;
+
+    @RequestMapping("/")
+    public String main_page(
+            @RequestParam(required = false) String category,
+            @RequestParam(required = false) String roomId,
+            HttpServletRequest req,
+            HttpServletResponse res
+    ) {
+        if(category == null){
+            category = "index";
+        }
+        req.setAttribute("page","page/"+category);
+        req.setAttribute("roomlist",chatRoomService.chatRoomList());
+        if(roomId != null){
+            req.setAttribute("roomInfo",chatRoomService.findChatRoom(Long.parseLong(roomId)));
+        }
+         //방 목록들을 제공해야한다.
+        return "index";
+    }
+
+
 }
